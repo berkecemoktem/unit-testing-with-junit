41d9c0cf2d003bb79dffca813a1c79fd403e2831
berkecemoktem <berkecemoktem@hotmail.com>
2024-03-16 16:18:23 +0300

    chore: test suits added

---
+++ src/test/java/CredentialsTestSuit.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        CustomerCredentialsServiceImplTest.class
+})
+
+public class CredentialsTestSuit {
+}
---
+++ src/test/java/ShoppingSuit.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        ShoppingServiceImplTest.class
+})
+public class ShoppingSuit {
+
+}
